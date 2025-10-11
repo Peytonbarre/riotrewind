e4cd70d1f00022b8462f4ee1f24b9f8484f330ca
Peytonbarre <peypeyozzy@gmail.com>
2025-10-11 17:03:30 -0500

    box for easier view

--- rift-rewind/src/App.jsx
+++ rift-rewind/src/App.jsx
@@ -1,18 +1,31 @@
 import './App.css'
-import { Canvas, extend } from '@react-three/fiber'
+import { Canvas, extend, useFrame } from '@react-three/fiber'
 import { Vector3 } from 'three/src/Three.Core.js'
 import { MeshLineGeometry, MeshLineMaterial} from 'meshline'
+import { easing } from 'maath'
 
 extend({MeshLineGeometry, MeshLineMaterial})
 
 export default function App() {
   return (
     <Canvas camera={{position: [0, 0, 5], fov:90}}>
-      <color attach="background" args={['#101020']}/>å
+      <color attach="background" args={['#101020']}/>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[10, 10, 5]} intensity={1} />
+      <CameraRig/>
       <mesh>
-        <meshLineGeometry points={[-1, 1, -1, 0, 1, 0, 1, 1, 1]}/>
-        <meshLineMaterial width={0.5} color={'#fe3d00'}/>
+        <boxGeometry args={[2, 2, 2]} />
+          {['red', 'blue', 'green', 'yellow', 'purple', 'orange'].map((color, i) => (
+            <meshStandardMaterial key={i} attach={`material-${i}`} color={color} />
+          ))}
       </mesh>
     </Canvas>
   )
+}
+
+function CameraRig({radius=20}){
+  useFrame((state, dt) => {
+    // easing.damp3(state.camera.position, [Math.sin(state.pointer.x) * radius, Math.atan(state.pointer.y) * radius, Math.cos(state.pointer.x) * radius], 0.25, dt)
+    easing.damp3(state.camera.position, [Math.cos(state.pointer.y) * Math.sin(state.pointer.x) * radius,Math.sin(state.pointer.y) * Math.sin(state.pointer.x) * radius, Math.cos(state.pointer.x) * radius], 0.25, dt)
+  })
 }
