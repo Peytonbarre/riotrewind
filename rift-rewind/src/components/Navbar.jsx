d89f6a6fc2c8133fe1136bb5d1921496834863dc
Peytonbarre <peypeyozzy@gmail.com>
2025-10-11 20:45:05 -0500

    navbar positioning

--- rift-rewind/src/components/Navbar.jsx
+++ rift-rewind/src/components/Navbar.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react'
 import './Navbar.css'
 
 export default function Navbar() {
+    const [summonerSearch, setSummonerSearch] = useState('')
+
+    const handleSearchInputChange = (e) => {
+        setSummonerSearch(e.target.value);
+    }
+
     return(
     <div className="navbar">
         {/* search */}
         <div className='midnav'>
-            <h2>search</h2>
+            <div className='searchbar'>
+                <input
+                    type="text"
+                    placeholder="Search Summoner"
+                    value={summonerSearch}
+                    onChange={handleSearchInputChange}
+                />
+            </div>
         </div>
         {/* login + settings + friends */}
         <div className='rightnav'>
